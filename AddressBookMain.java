4030f8ecf8cfba3bbdf6eea84d7f615242d6167f
Padmajase <padmajapawar7@gmail.com>
2022-04-18 23:14:05 -0700

    Added : AddressBook program UC1

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,9 +1,32 @@
-/** printing welcome message **/
+import java.util.Scanner;
+import java.util.*;
 
 class AddressBookMain
 {
 	public static void main(String args[])
 	{
 		System.out.println("Welcome To AddressBook !");
+		Contacts.createContact("Padmaja Eknarayan", "IT park chakan", "pune", "maharashtra", "430501", "9067679078", "opsdsnmn@gmail.com");
+	}
+}
+
+class Contacts
+{
+	/**creating contact using Arraylist **/
+
+	static void createContact(String name, String address, String city, String state, String zipCode, String phoneNo, String email)
+	{
+		ArrayList<String> firstContact = new ArrayList<String>(7);
+				
+		firstContact.add(name);
+		firstContact.add(address);
+		firstContact.add(city);
+		firstContact.add(state);
+		firstContact.add(zipCode);
+		firstContact.add(phoneNo);
+		firstContact.add(email);
+			
+		System.out.println(firstContact );
+		
 	}
 }
