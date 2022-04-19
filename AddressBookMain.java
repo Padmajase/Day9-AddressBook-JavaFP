269899c87d983e3a2f169181c8ff80fdba831527
Padmajase <padmajapawar7@gmail.com>
2022-04-18 23:19:28 -0700

    Added : AddressBook program UC4

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 class AddressBookMain
 {
 	public static void main(String args[])
 	{
 		System.out.println("	 Welcome To AddressBook !");
-		System.out.println("	***********************");
+		System.out.println("	**************************");
 		
-		Contacts.createContact();
+		Contacts.deleteContact();
 	}
 }
 
 class Contacts
 {
-	static void createContact()
+	static void deleteContact()
 	{
 		ArrayList<String> firstContact = new ArrayList<String>(7);
 		Scanner sc = new Scanner(System.in);
@@ -23,7 +24,8 @@ class Contacts
 		System.out.println("Enter the contact details  : ");
 		System.out.println("name address city state zip phoneNo email  : ");
 		
-		/** adding contact details from console **/
+		/**adding contact using for loop and Arraylist **/
+
 		for(int i =0; i<7; i++)
 		{
 			String s = sc.nextLine();
@@ -34,56 +36,18 @@ class Contacts
 		System.out.println(firstContact );
 	
 		System.out.println(" ");
-		System.out.println("Enter which detail you have to edit  : ");
-		System.out.println("name or address or city or state or zip or phone or email  : ");
-		
-		/** editing specific detail which user want **/
-		String detailToEdit = sc.nextLine();
-		
-			switch (detailToEdit) {
-			case "name" :
-				System.out.println("Enter the name to edit  : ");
-				String n = sc.nextLine();
-				firstContact.set(0, n);
-				break;
-			case "address" :
-				System.out.println("Enter the address to edit  : ");
-				String a = sc.nextLine();
-				firstContact.set(1, a);
-				break;
-			case "city" :
-				System.out.println("Enter the name to edit  : ");
-				String city = sc.nextLine();
-				firstContact.set(2, city);
-				break;
-			case "state" :
-				System.out.println("Enter the name to edit  : ");
-				String state = sc.nextLine();
-				firstContact.set(3, state);
-				break;
-			case "zip" :
-				System.out.println("Enter the name to edit  : ");
-				String zip = sc.nextLine();
-				firstContact.set(5, zip);
-				break;
-			case "phone" :
-				System.out.println("Enter the name to edit  : ");
-				String phone = sc.nextLine();
-				firstContact.set(6, phone);
-				break;
-			case "email" :
-				System.out.println("Enter the name to edit  : ");
-				String email = sc.nextLine();
-				firstContact.set(7, email);
-				break;
-			default :
-				System.out.println("no edit match  : ");
-				
-			}
-			
-			System.out.println("----------------------------------------------------- ");
-			System.out.println("edited contact details are :");
-			System.out.println(firstContact );
+		System.out.println("Enter the name of contact that have to delete  : ");
+		String nameToDelete = sc.nextLine();
+	
+		/**deleting the added contact using Arraylist method **/
+	
+		if(nameToDelete.equals(firstContact.get(0)))
+		{
+			firstContact.removeAll(firstContact);
+		}
+		System.out.println(" ");
+		System.out.println("contact deleted successfuly ");
+		System.out.println(firstContact );
 		
 	}
 }
